410847b93625a1f962bd052042659ab5b676b8a5
Chris Barry <2490974+ChrisW-B@users.noreply.github.com>
2022-01-02 11:51:21 -0500

    prep for pi

--- nowplaying_impression/__main__.py
+++ nowplaying_impression/__main__.py
@@ -1,31 +1,41 @@
-import requests
+# from inky.inky_uc8159 import Inky
 from PIL import Image
-from inky.inky_uc8159 import Inky
+from spotipy.oauth2 import SpotifyOAuth
 import hitherdither
+import spotipy
+import os
 
-inky = Inky()
+# inky = Inky()
 saturation = 0.8
 thresholds = [64, 64, 64]
-url = "http://api.chriswb.dev/"
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.environ['SPOTIFY_CLIENT_ID'],
+    client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
+    redirect_uri='http://127.0.0.1:5000',
+    open_browser=False,
+    scope='user-read-currently-playing user-top-read'
+))
 
 palette = hitherdither.palette.Palette(
     inky._palette_blend(saturation, dtype='uint24'))
 
-payload = "{\"query\":\"{\\n  lastfm {\\n    nowplaying {\\n      title\\n      artist\\n      image\\n      nowplaying\\n      id\\n    }\\n  }\\n}\\n\"}"
-headers = {"Content-Type": "application/json"}
 
-response = requests.request("POST", url, data=payload, headers=headers)
+def write_image(image_url):
+    bg = Image.new("RGBA", (inky.WIDTH, inky.HEIGHT), (255, 255, 255, 255))
+    img = Image.open(requests.get(image_url, stream=True).raw).convert("RGB")
+    image_dithered = hitherdither.ordered.cluster.cluster_dot_dithering(
+        img, palette, thresholds, order=8)
+    bg.paste(image_dithered, (150, 94))
+    inky.set_image(bg)
+    inky.show()
 
-if response.status_code == requests.codes.ok:
-    json = response.json()
-    track = json['data']['lastfm']['nowplaying']
-    if track['nowplaying'] == True:
-        bg = Image.new("RGBA", (inky.WIDTH, inky.HEIGHT), (255,255,255,255))
-        img = Image.open(requests.get(track['image'], stream=True).raw).convert("RGB")
-        # image_dithered = hitherdither.ordered.bayer.bayer_dithering(img, palette, thresholds, order=8)
-        image_dithered = hitherdither.ordered.cluster.cluster_dot_dithering(img, palette, thresholds, order=8)
-        bg.paste(image_dithered, (150, 94))
-        inky.set_image(bg)
-        inky.show()
-else:
-        response.raise_for_status()
+
+def main():
+    now_playing = sp.current_user_playing_track()
+    if now_playing['is_playing']:
+        print(now_playing['item']['album']['images'][0]['url'])
+
+
+main()
